19f5c32c298ab623a22907d1e186ce9ae805b730
Thiago Giammattey <giammattey@gmail.com>
2022-01-12 18:21:29 -0300

    add home page

---
+++ .eleventy.js
@@ -0,0 +1,32 @@
+const sortByDisplayOrder = require('./src/utils/sort-by-display-order.js');
+
+// Eleventy config file
+module.exports = config => {
+  // Set directories to pass through to the dist folder
+  config.addPassthroughCopy('./src/images/');
+
+  // Returns work items, sorted by display order
+  config.addCollection('work', collection => {
+    return sortByDisplayOrder(collection.getFilteredByGlob('./src/work/*.md'));
+  });
+
+  // Returns work items, sorted by display order then filtered by featured
+  config.addCollection('featuredWork', collection => {
+    return sortByDisplayOrder(collection.getFilteredByGlob('./src/work/*.md')).filter(
+      x => x.data.featured
+    );
+  });
+
+
+  // Return your Object options
+  return {
+  // We’re telling Eleventy that markdown files, data files and HTML files should be processed by Nunjucks. That means that we can now use .html files instead of having to use .njk files
+  markdownTemplateEngine: 'njk',
+  dataTemplateEngine: 'njk',
+  htmlTemplateEngine: 'njk',
+    dir: {
+      input: 'src',
+      output: 'dist'
+    }
+  };
+};
